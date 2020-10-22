1fb49cc52d421b73063166b19beb1f0b98d56ca4
Caner Koçaş <47156830+Canerkocas@users.noreply.github.com>
2020-10-22 22:14:15 +0300

    header button added

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -35,7 +35,11 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden",
+    elevation: 5,
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
   },
   container: {
     flex: 1,
@@ -44,7 +48,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
@@ -19,6 +21,17 @@ MealDetailScreen.navigationOptions = () => {
 
   return {
     headerTitle: selectedMeal.title,
+    heeaderRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("icon");
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
