5a741b4ba8727e215d30d823aa789542bcf4ad95
Caner Koçaş <47156830+Canerkocas@users.noreply.github.com>
2020-10-22 21:46:32 +0300

    meals detail screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,23 +1,52 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
-const CategoriesMealsScreen = (props) => {
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+const CategoryMealScreen = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
+      />
+    );
+  };
+
   const catId = props.navigation.getParam("categoryId");
 
-  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
   return (
     <View style={styles.screen}>
-      <Text>Text CategoriesMealsScreen</Text>
-      <Text>Selected {selectedCategory.title}</Text>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
+      />
     </View>
   );
 };
 
-CategoriesMealsScreen.navigationOptions = (navigationData) => {
+CategoryMealScreen.navigationOptions = (navigationData) => {
   const catId = navigationData.navigation.getParam("categoryId");
 
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
   return {
     headerTitle: selectedCategory.title,
   };
@@ -25,11 +54,11 @@ CategoriesMealsScreen.navigationOptions = (navigationData) => {
 
 const styles = StyleSheet.create({
   screen: {
-    width: 100 + "%",
-    height: 100 + "%",
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 15,
   },
 });
 
-export default CategoriesMealsScreen;
+export default CategoryMealScreen;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
 const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam("mealId");
+
+  const selectedMeal = MEALS.filter((meal) => meal.id === mealId);
   return (
     <View style={styles.screen}>
-      <Text>Text MealDetailScreen</Text>
+      <Text>{selectedMeal.title}</Text>
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = () => {
+  const mealId = props.navigation.getParam("mealId");
+  const selectedMeal = MEALS.filter((meal) => meal.id === mealId);
+
+  return {
+    headerTitle: selectedMeal.title,
+  };
 };
 
 const styles = StyleSheet.create({
